b910fa9c3c5771d6b747f2caf0c56457f27ef677
The Natural <philip.smalls86@gmail.com>
2019-06-01 19:32:30 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:dennis_ritchie][:languages].join(" ")
 end
 
 def adding_matz
@@ -80,7 +84,13 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = 
+    {
+          :known_for => "Ruby", 
+          :languages => ["LISP", "C"]
+     }
+          
+  programmer_hash
 end
 
 def changing_alan
@@ -102,6 +112,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash
      
 end
 
@@ -122,4 +134,6 @@ def adding_to_dennis
         }
      }
 
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
