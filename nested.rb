d437a23884911851609b0ef8f4820086714ae555
The Natural <philip.smalls86@gmail.com>
2019-06-01 18:58:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,12 +84,12 @@ def adding_matz
         }
      }
 
-     programmer_hash = 
+     programmer_hash[ 
         :yukihiro_matsumoto => 
         {
           :known_for => "Ruby",
           :languages => ["LISP", "C"]
- }
+ }]
 end
 
 def changing_alan
